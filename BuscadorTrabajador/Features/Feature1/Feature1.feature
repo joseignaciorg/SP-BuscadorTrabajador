4ecf6899258917e2b21ca2f54e0952bc8e17e531
Nacho <joseignacio.rodriguez@tajamar365.com>
2016-05-05 10:56:37 +0000

    Aplicacion que busca empleados.

---
+++ BuscadorTrabajador/Features/Feature1/Feature1.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="6484606c-c934-43e8-99d6-8744e2623984" description="SharePoint Add-in Feature" featureId="6484606c-c934-43e8-99d6-8744e2623984" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="BuscadorTrabajador Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="922bcd38-ff98-4a68-984f-f2bc768496ca" />
+    <projectItemReference itemId="3e46711f-8b43-40b3-9809-40a1c95d4d0b" />
+    <projectItemReference itemId="076b7f57-db91-4f71-a665-d1a8245f1ec2" />
+    <projectItemReference itemId="efec85f8-58be-4cd6-969e-b60fab37403d" />
+    <projectItemReference itemId="5e88402b-2914-4428-a4ee-66dfea1638f3" />
+    <projectItemReference itemId="d359ffdb-8b02-46c1-a633-8040361b0b2d" />
+  </projectItems>
+</feature>
---
+++ BuscadorTrabajador/Scripts/App.js
@@ -0,0 +1,56 @@
+﻿'use strict';
+
+function getPersonas() {
+    var nom = $("#txtNombre").val();
+
+    //vacio la capa de resultado
+    $("#resultado").empty();
+
+    //recupero la lista para recuperar el nombre del trabajador
+    var uri = _spPageContextInfo.webAbsoluteUrl + "/_api/lists/getByTitle('Trabajador')/items?$filter=nombre eq '" + nom + "'";
+
+    //llamada ajax para recuperar
+    $.ajax({
+        type: "GET",
+        url: uri,
+        contenType: "application/json",
+        headers: {
+            "accept":"application/json;odata=verbose"
+        },
+        success:onGetresults,
+        error:onError
+    });
+}
+
+function onError() {
+    alert(r.status);
+}
+
+function onGetresults(xhr) {
+    var odata = xhr.d.results;
+    var res = "<table>" +
+        "<thead>" +
+        "<th>Nombre</th>" +
+        "<th>Apellidos</th>" +
+        "<th>Edad</th>" +
+        "<th>Salario</th>" +
+        "<th>Alta</th>" +
+        "</thead>";
+    $.each(odata, function(i, item) {
+        res += "<tr>";
+        res += "<td>" + item.nombre + "</td>";
+        res += "<td>" + item.apellidos + "</td>";
+        res += "<td>" + item.edad + "</td>";
+        res += "<td>" + item.salario + "</td>";
+        res += "<td>" + item.alta + "</td>";
+        res += "</tr>";
+    });
+    res += "</table>";
+    $("#resultado").html(res);
+}
+
+$(document).ready(function() {
+    $("#btnOK").click(getPersonas);
+});
+
+
